35450fb0c6dda1fe5795a9740fff40daea431184
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-15 17:48:48 +0100

    refactored broken content

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
-import { StyledCardSection } from "../components/CardSection/StyledCardSection";
-import Footer from "../components/Footer/Footer";
+import { StyledCardSection } from "../components/StyledCardSection";
+import Footer from "../components/Footer";
 import { StyledMain } from "../components/GlobalStyles";
-import Header from "../components/Header/Header";
-import { initialEvents, Eventlist } from "../data/events";
+import Header from "../components/Header";
+import { initialEvents } from "../data/initialEvents";
+import Link from "next/link";
+import StyledLink from "../components/GlobalStyles";
 
 export default function Schedule() {
-  const [events, setEvents] = useState(initialEvents);
-  const [favorites, setFavorites] = useState([]);
 
   return (
     <>
       <Header />
       <StyledMain>
-        <StyledCardSection>{Eventlist}</StyledCardSection>
+        <StyledCardSection>
+    {initialEvents.map((event) => (
+      <Link href="/" key={event.id}>
+        <article>
+          <h3 key={event.id}>{event.name}</h3>
+          <div>
+            <p>{event.date}</p>
+            <p>{event.category}</p>
+          </div>
+        </article>
+      </Link>
+    ))}
+</StyledCardSection>
       </StyledMain>
       <Footer />
     </>
