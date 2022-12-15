5ac2b15bded02de69e6411890e66cd67f491b28d
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-15 16:05:14 +0100

    delet duplicate data file

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,9 @@ import { StyledCardSection } from "../components/CardSection/StyledCardSection";
 import Footer from "../components/Footer/Footer";
 import { StyledMain } from "../components/GlobalStyles";
 import Header from "../components/Header/Header";
-import { initialEvents, Eventlist } from "../data/events";
+import { Eventlist } from "../data/events";
 
 export default function Schedule() {
-  const [events, setEvents] = useState(initialEvents);
-  const [favorites, setFavorites] = useState([]);
-
   return (
     <>
       <Header />
