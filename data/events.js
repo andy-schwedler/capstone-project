5a78dca1edcd79656f01e8d84bb73a3e03d537de
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-15 19:16:18 +0100

    delete header directory

--- data/events.js
+++ data/events.js
@@ -1,6 +1,4 @@
 import { nanoid } from "nanoid";
-import { StyledLink } from "../components/GlobalStyles";
-import BookmarkButton from "../components/Icons/BookmarkButton";
 
 export const initialEvents = [
   { id: nanoid(), name: "Beaverletics", date: "24.12.2023", category: "easy" },
@@ -21,20 +19,3 @@ export const initialEvents = [
   { id: nanoid(), name: "Beaverletics", date: "29.12.2023", category: "hard" },
   { id: nanoid(), name: "Beaverletics", date: "30.12.2023", category: "gold" },
 ];
-
-export const Eventlist = (
-  <>
-    {initialEvents.map((event) => (
-      <StyledLink href="/" key={event.id}>
-        <article>
-          <BookmarkButton />
-          <h3 key={event.id}>{event.name}</h3>
-          <div>
-            <p>{event.date}</p>
-            <p>{event.category}</p>
-          </div>
-        </article>
-      </StyledLink>
-    ))}
-  </>
-);
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import { StyledCardSection } from "../components/CardSection/StyledCardSection";
 import Footer from "../components/Footer";
-import { StyledMain } from "../components/GlobalStyles";
-import Header from "../components/Header/Header";
-import { Eventlist } from "../data/events";
+import { StyledLink, StyledMain } from "../components/GlobalStyles";
+import Header from "../components/Header";
+import { initialEvents } from "../data/events";
 
 export default function Schedule() {
   return (
     <>
       <Header />
       <StyledMain>
-        <StyledCardSection>{Eventlist}</StyledCardSection>
+        <StyledCardSection>
+          {initialEvents.map((event) => (
+            <StyledLink href="/" key={event.id}>
+              <article>
+                <h3 key={event.id}>{event.name}</h3>
+                <div>
+                  <p>{event.date}</p>
+                  <p>{event.category}</p>
+                </div>
+              </article>
+            </StyledLink>
+          ))}
+        </StyledCardSection>
       </StyledMain>
       <Footer />
     </>
