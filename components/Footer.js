b99102eedefe0d66cd426645d648c62c836b3123
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-19 21:25:21 +0100

    refactor button component

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styled from "styled-components";
-import { BookmarkIcon } from "./BookmarkButton";
+import { BookmarkIcon } from "./Icons";
 import ScheduleIcon from "./Icons";
 
 export default function Footer() {
