4e83ac877eb0d8c96428d7d314f1c0941a2fe67e
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-20 21:50:06 +0100

    touch dummy data

--- data/sampleEvents.js
+++ data/sampleEvents.js
@@ -2,58 +2,65 @@ import { nanoid } from "nanoid";
 
 export const initialEvents = [
   {
-    id: nanoid(),
+    id: 1,
     name: "Beaverletics",
     date: "24.12.2023",
-    category: "sport",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: false,
     location: "München",
   },
   {
-    id: nanoid(),
+    id: 2,
     name: "Beaver Kids",
     date: "25.12.2023",
-    category: "children",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: true,
     location: "München",
   },
   {
-    id: nanoid(),
+    id: 3,
     name: "Beaver brush teeth",
     date: "26.12.2023",
-    category: "hard",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: false,
     location: "München",
   },
   {
-    id: nanoid(),
+    id: 4,
     name: "Anonymous Beavers",
     date: "27.12.2023",
-    category: "health",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: false,
     location: "München",
   },
   {
-    id: nanoid(),
+    id: 5,
     name: "Yoga",
     date: "28.12.2023",
-    category: "sport",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: false,
     location: "München",
   },
   {
-    id: nanoid(),
+    id: 6,
     name: "Floating down the river",
     date: "29.12.2023",
-    category: "gold",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: false,
     location: "München",
   },
   {
-    id: nanoid(),
+    id: 7,
     name: "Beaverletics",
     date: "30.12.2023",
-    category: "gold",
+    memory:
+      "Dolore id et excepteur non veniam laborum. Reprehenderit magna tempor incididunt proident irure eiusmod in do. Laboris laboris consequat voluptate veniam enim consectetur mollit elit do. Nostrud enim commodo sit fugiat minim quis. Nostrud et officia id voluptate ullamco laborum nostrud ullamco.Id aute proident do nulla Lorem consectetur. Deserunt mollit pariatur ullamco incididunt id labore pariatur ut sint commodo dolore duis consequat. Officia ipsum ipsum consequat non qui pariatur duis eu nisi. Sit excepteur anim sit non id eu. Ipsum non consequat do exercitation. Duis cillum pariatur officia non ad laboris labore dolor reprehenderit consequat ad Lorem. Cupidatat labore minim aliqua aliquip. Deserunt adipisicing dolor laborum ipsum ad ut culpa officia duis irure. Cupidatat labore reprehenderit eu ex veniam est. Eiusmod ad non deserunt in sit consequat et mollit aliqua. Commodo tempor est officia laborum et culpa voluptate sunt labore eiusmod irure reprehenderit nisi minim. Ad quis proident pariatur in ut fugiat.",
     isFavorite: false,
     location: "München",
   },
