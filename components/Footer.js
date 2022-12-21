302ad1357bb38f3957b56233c5e293bf6e556cd1
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-21 11:09:17 +0100

    add create button to footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,62 +1,48 @@
-import Link from "next/link";
 import styled from "styled-components";
-import { BookmarkIcon } from "./Icons";
+import { BookmarkIcon, CreateIcon } from "./Icons";
 import ScheduleIcon from "./Icons";
+import { StyledLink } from "./GlobalStyles";
 
 export default function Footer() {
   return (
     <StyledFooterList>
-      <StyledIconContainer>
-        <Link href={"/"}>
-          <ScheduleIcon
-            aria-label="overview"
-            alt="overview"
-            fill="var(--beaver)"
-            width={40}
-            height={40}
-          />
-        </Link>
-      </StyledIconContainer>
-      <StyledIconContainer>
-        <Link href={"/bookmarks"}>
-          <BookmarkIcon
-            aria-label="bookmark"
-            alt="bookmark"
-            color="#245669"
-            width={40}
-            height={40}
-          />
-        </Link>
-      </StyledIconContainer>
+      <StyledLink href={"/"}>
+        <ScheduleIcon
+          aria-label="overview"
+          alt="overview"
+          fill="var(--beaver)"
+          width={40}
+          height={40}
+        />
+      </StyledLink>
+      <StyledLink href={"/bookmarks"}>
+        <CreateIcon
+          aria-label="bookmark"
+          alt="bookmark"
+          color="var(--beaver)"
+          width={40}
+          height={40}
+        />
+      </StyledLink>
+      <StyledLink href={"/bookmarks"}>
+        <BookmarkIcon
+          aria-label="bookmark"
+          alt="bookmark"
+          color="var(--beaver)"
+          width={40}
+          height={40}
+        />
+      </StyledLink>
     </StyledFooterList>
   );
 }
 
 const StyledFooterList = styled.footer`
-  list-style-type: none;
-  background-color: lightblue;
-  width: 100vw;
   display: flex;
-  margin: 0;
-  padding: 5px;
   justify-content: space-around;
-  align-items: center;
-`;
-
-const StyledIconContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: #245669;
-  font-weight: bold;
-  height: auto;
-  margin-bottom: 2px;
-`;
-
-export const StyledButton = styled.button`
-  background-color: transparent;
-  border: none;
-  padding: 0;
+  background-color: var(--beaver1);
+  list-style-type: none;
   margin: 0;
+  padding: 1%;
+  align-items: center;
 `;
