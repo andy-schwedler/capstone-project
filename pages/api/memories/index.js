9dd7ad4bd224dd6111d8db603ea8d140472ea898
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-29 17:14:35 +0100

    fix get method

--- pages/api/memories/index.js
+++ pages/api/memories/index.js
@@ -33,4 +33,14 @@ export default async function handler(req, res) {
 
     res.status(200).json(memoriesArray);
   }
+
+  if (request.method === "POST") {
+    const data = request.body;
+    try {
+      const newQuestion = await Question.create(data);
+      response.status(201).json(newQuestion);
+    } catch (error) {
+      response.status(400).json("Data could not be processed", { error });
+    }
+  }
 }
