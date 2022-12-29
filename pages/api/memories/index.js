34829268b4c67e43f9278fa3ea9c2fd9e5e33f18
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-29 14:32:15 +0100

    fix server api

--- pages/api/memories/index.js
+++ pages/api/memories/index.js
@@ -1,21 +1,36 @@
 import dbConnect from "../../../db/dbConnect";
-import Memory from "../../../db/model/Memory";
+import Memory from "../../../db/models/Memory";
 
 export default async function handler(req, res) {
   //1. Connect to the database and wait for the connection to be established
   await dbConnect();
 
-  // 2. Read data from database (async! use await!)
-  const memories = await Memory.find(); // this is a mongoose collection
+  //   switch (req.method) {
+  //     case "GET":
+  //       try {
+  //         const filter = {};
+  //         const memories = await Memory.find(filter); // this is a mongoose collection;
+  //         res.status(200).json(memories);
+  //       } catch (error) {
+  //         res.status(500).json({ error: "something went wrong" });
+  //       }
+  //       break;
+  //     default:
+  //       res.status(405).json({ error: "method not allowed" });
+  //   }
+  if (req.method === "GET") {
+    // 2. Read data from database (async! use await!)
+    const memories = await Memory.find(); // this is a mongoose collection
 
-  // 3. Translate collection to array
-  const memoriesArray = memories.map((memory) => {
-    return {
-      id: memory._id,
-      name: memory.name,
-      date: memory.date,
-    };
-  });
+    // 3. Translate collection to array
+    const memoriesArray = memories.map((memory) => {
+      return {
+        id: memory._id,
+        name: memory.name,
+        date: memory.date,
+      };
+    });
 
-  res.status(200).json(memoriesArray);
+    res.status(200).json(memoriesArray);
+  }
 }
