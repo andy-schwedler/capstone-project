53b9131f26a62171fa4853af4e45848394707b3a
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2023-01-12 08:44:01 +0100

    fix search

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,69 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { StyledMain, StyledGridWrapper } from "../components/GlobalStyles";
 import MemoryListCard from "../components/MemoryCard/MemoryListCard";
-import InputAndLabelTextPair from "../components/Forms/Input/InputAndLabelTextPair";
+
 import SearchBar from "../components/SearchBar";
+
+import SortingBar from "../components/SortingBar";
 
 export default function Overview({
   sampleEvents,
   onToggleFavorite,
   onDelete,
   onEditMemory,
+  onAscendingSort,
+  onDescendingSort,
 }) {
   const [searchTerm, setSearchTerm] = useState([]);
-  const [sortOrder, setSortOrder] = useState();
 
-  function handleSearch() {
+  function handleSearch(event) {
     const searchEntry = event.target.value;
 
     const filteredDetails = sampleEvents.filter((sampleEvent) => {
-      return (
-        sampleEvent.details.toLowerCase().includes(searchEntry.toLowerCase()) ||
-        sampleEvent.headline.toLowerCase().includes(searchEntry.toLowerCase())
-      );
+      if (searchEntry === "") {
+        return null;
+      } else
+        return (
+          sampleEvent.details
+            .toLowerCase()
+            .includes(searchEntry.toLowerCase()) ||
+          sampleEvent.headline.toLowerCase().includes(searchEntry.toLowerCase())
+        );
     });
     setSearchTerm(filteredDetails);
   }
 
-  const filteredResults = searchTerm?.map((sampleEvent) => (
+  const searchResults = searchTerm?.map((sampleEvent) => (
+    <Fragment key={sampleEvent.id}>
+      <MemoryListCard
+        sampleEvent={sampleEvent}
+        onToggleFavorite={onToggleFavorite}
+        onDelete={onDelete}
+        onEditMemory={onEditMemory}
+      />
+    </Fragment>
+  ));
+  // unused code for now
+  // const sortedList = sampleEvents
+  //   ?.slice()
+  //   .sort((a, b) => {
+  //     const date1 = new Date(a.date);
+  //     const date2 = new Date(b.date);
+  //     // ascending / oldest first
+  //     return date1 - date2;
+  //   })
+  //   .map((sampleEvent) => (
+  //     <Fragment key={sampleEvent.id}>
+  //       <MemoryListCard
+  //         sampleEvent={sampleEvent}
+  //         onToggleFavorite={onToggleFavorite}
+  //         onDelete={onDelete}
+  //         onEditMemory={onEditMemory}
+  //       />
+  //     </Fragment>
+  //   ));
+
+  const allMemories = sampleEvents?.map((sampleEvent) => (
     <Fragment key={sampleEvent.id}>
       <MemoryListCard
         sampleEvent={sampleEvent}
@@ -38,34 +76,17 @@ export default function Overview({
     </Fragment>
   ));
 
-  const sortedList = sampleEvents
-    ?.slice()
-    .sort((a, b) => {
-      const date1 = new Date(a.date);
-      const date2 = new Date(b.date);
-
-      return date1 - date2;
-    })
-    .reverse()
-    .map((sampleEvent) => (
-      <Fragment key={sampleEvent.id}>
-        <MemoryListCard
-          sampleEvent={sampleEvent}
-          onToggleFavorite={onToggleFavorite}
-          onDelete={onDelete}
-          onEditMemory={onEditMemory}
-        />
-      </Fragment>
-    ));
-
   return (
     <>
       <StyledGridWrapper>
         <Header />
         <StyledMain>
-          <button>sort</button>
+          <SortingBar
+            onAscendingSort={onAscendingSort}
+            onDescendingSort={onDescendingSort}
+          />
           <SearchBar onSearch={handleSearch} />
-          {filteredResults?.length === 0 ? sortedList : filteredResults}
+          {searchResults?.length === 0 ? allMemories : searchResults}
         </StyledMain>
         <Footer sampleEvents={sampleEvents} />
       </StyledGridWrapper>
