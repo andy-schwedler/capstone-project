d827b1ed7b5396cae2a36d99bc97ce3f587a6da6
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2023-01-16 13:51:16 +0100

    found more pieces

--- pages/createpage/index.js
+++ pages/createpage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import CreateMemoryForm from "../../components/Forms/CreateMemoryForm";
-import { StyledGridWrapper, StyledMain } from "../../components/GlobalStyles";
+import { StyledMain } from "../../components/GlobalStyles";
 import Header from "../../components/Header/Header";
 
 export default function CreatePage({ sampleEvents, onAddCreateCard }) {
