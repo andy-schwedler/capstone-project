f50abf06c511ee5d788df5df93daef0a3a2da98d
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2023-01-16 13:49:49 +0100

    hot fix

--- pages/createpage/index.js
+++ pages/createpage/index.js
@@ -6,12 +6,12 @@ import Header from "../../components/Header/Header";
 
 export default function CreatePage({ sampleEvents, onAddCreateCard }) {
   return (
-    <StyledGridWrapper>
+    <>
       <Header />
       <StyledMain>
         <CreateMemoryForm onAddCreateCard={onAddCreateCard} />
       </StyledMain>
       <Footer sampleEvents={sampleEvents} />
-    </StyledGridWrapper>
+    </>
   );
 }
